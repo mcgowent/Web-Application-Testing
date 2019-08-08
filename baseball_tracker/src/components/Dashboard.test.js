b7fc090b92ac618bba6a0388bd1137707b49d7f6
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-08 14:35:58 -0700

    Set up all method tests

--- baseball_tracker/src/components/Dashboard.test.js
+++ baseball_tracker/src/components/Dashboard.test.js
@@ -6,13 +6,38 @@ import '@testing-library/react/cleanup-after-each'
 import { strike, ball, foul, hit } from './Dashboard'
 
 describe('Dashboard.js', () => {
-
+    /*========================  strike() ================================ */
     describe('strike()', () => {
         it('Should Add +1 on event trigger', () => {
 
             expect(strike(0)).toBe(1)
         })
     })
+    /*============w============  ball() ================================ */
+
+    describe('ball()', () => {
+        it('Should Add +1 on event trigger', () => {
+
+            expect(ball(0)).toBe(1)
+        })
+    })
+    /*========================  foul() ================================ */
+
+    describe('foul()', () => {
+        it('Should Add +1 on event trigger', () => {
+
+            expect(foul(0)).toBe(1)
+        })
+    })
+    /*========================  hit() ================================ */
+
+    describe('hit()', () => {
+        it('Should return true', () => {
+
+            expect(hit()).toBe(true)
+        })
+    })
+
 })
 
 
