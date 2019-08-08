1928c952bbea80d43884eb1ea4daf6bd049416d3
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-08 16:58:52 -0700

    Testing is finally working, can finally get start

--- baseball_tracker/src/components/Dashboard.js
+++ baseball_tracker/src/components/Dashboard.js
@@ -2,8 +2,7 @@
 // - there is ** NO ** need to specify the type of hit (single, double, etc).
 // - changes recorded on this component should update the information shown by the`Display` component.
 
-import React, { useState } from 'react'
-import App from '../../../../React-Github-User-Card/github-cards/src/App';
+import React from 'react'
 
 
 
@@ -11,49 +10,64 @@ class Dashboard extends React.Component {
     /*========================States================================ */
     constructor() {
         super()
-        this.state = {
-            player: [{
-                name: "",
+        this.state =
+            {
                 strikes: 0,
                 balls: 0,
-                foul: 0,
+                fouls: 0,
                 hit: true
-            }]
-        }
+            }
     }
 
     /*========================Methods================================ */
-    strike = (e) => {
+    strike = () => {
+        this.setState(state => ({
+            strikes: state.strikes + 1
+        }))
+        return this.state.strike
         // Adds +1 to the display Strike counter
         // If Strikes total ever reaches 3 then reset both the Strikes and
         // and Ball counter to zero
-
-
     }
 
     ball = (e) => {
+        this.setState(state => ({
+            balls: state.balls + 1
+        }))
         // Adds +1 to the display ball counter
         // If Ball total ever reaches 4 then reset both the Strikes and
         // and Ball counter to zero
     }
 
     foul = (e) => {
+        this.setState(state => ({
+            fouls: state.fouls + 1
+        }))
         // Foul ball is worth +1 strike expect when the current state of
         // strike is equal to 2 or greater, then it is worth 0 strikes
     }
 
     hit = (e) => {
+        this.setState(state => ({
+            hit: !state.hit
+        }))
         // If a hit occures the scores for Balls and Strikes should be set to Zero
     }
     render() {
         return (
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
+            <div>
+                <button onClick={this.strike} data-testid="strike">Strike</button>
+                <p>Strikes:{this.state.strikes}</p>
+                <button onClick={this.ball}>Ball</button>
+                <p>{this.state.balls}</p>
+                <button onClick={this.foul}>Foul</button>
+                <p>{this.state.fouls}</p>
+                <button onClick={this.hit}>{this.state.hit ? 'True' : 'False'}</button>
+                {console.log(this.state.hit)}
+                <p></p>
+            </div>
         )
     }
-
 }
 
-export default App;
+export default Dashboard;
--- baseball_tracker/src/components/Dashboard.test.js
+++ baseball_tracker/src/components/Dashboard.test.js
@@ -1,62 +1,14 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import '@testing-library/react/cleanup-after-each'
+import React from "react"
+import { render, fireEvent } from '@testing-library/react'
+import "@testing-library/react/cleanup-after-each"
 
+import Dashboard from './Dashboard'
 
-import { strike, ball, foul, hit } from './Dashboard'
+it('display the count', () => {
+    const { getByTestId, findAllByText } = render(<Dashboard />)
 
-describe('Dashboard.js', () => {
-    /*========================  strike() ================================ */
-    describe('strike()', () => {
-        it('Should Add +1 on event trigger', () => {
+    const button = getByTestId(/strike/)
 
-            expect(strike(0)).toBe(1)
-        })
-    })
-    /*============w============  ball() ================================ */
-
-    describe('ball()', () => {
-        it('Should Add +1 on event trigger', () => {
-
-            expect(ball(0)).toBe(1)
-        })
-    })
-    /*========================  foul() ================================ */
-
-    describe('foul()', () => {
-        it('Should Add +1 on event trigger', () => {
-
-            expect(foul(0)).toBe(1)
-        })
-    })
-    /*========================  hit() ================================ */
-
-    describe('hit()', () => {
-        it('Should return true', () => {
-
-            expect(hit()).toBe(true)
-        })
-    })
-
+    fireEvent.click(button)
+    findAllByText(/Strikes:0/)
 })
-
-
-
-
-// describe('<Players />', () => {
-//     it('should render a list of players provided on props', () => {
-//         const playersData = [{
-//             name: "PlayerOne",
-//             strikes: 2,
-//             balls: 1,
-//             foul: 1,
-//             hit: false
-//         }
-//         ]
-//         const comp = render(<Dashboard player={playersData} />)
-
-//         const players = comp.getAllByTestId('player')
-
-//         expect(players).toHaveLength(playersData.length)
-//     })
-// })
